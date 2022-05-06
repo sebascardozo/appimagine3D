0fc90cd2f5be3d9e9bc30ba6ce6ea41642533e3c
Sebastian Cardozo <sebascardozo2020@gmail.com>
2022-05-06 09:34:52 -0300

    agregando estilos

--- src/components/ItemCounter/ItemCounter.jsx
+++ src/components/ItemCounter/ItemCounter.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import image from '../images/impresora3d.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import '../styles/ItemCounter.css'
 
 function ItemCounter ({stock, initial, onAdd}) {
@@ -25,12 +27,10 @@ function ItemCounter ({stock, initial, onAdd}) {
      <div className="ItemCounter">
          <img src={image} alt="" />
      <h1>Ipresora 3D</h1>
-       <p>Start editing to see some magic happen </p>
-      
       <button id='plus' onClick={increase}>Sumar</button>
            <span>{quantity}</span>
-     <button id='less' onClick={decrease}>Restar</button>
-     <button className="addToCart" onClick={addToCart}></button>
+     <button id='less' onClick={decrease}> Restar </button>
+     <button className="addToCart" onClick={addToCart}><FontAwesomeIcon icon={faCartShopping} /></button>
     </div>
      );
    }
