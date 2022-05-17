d406ad6b6ffdeff2f932a283e336562ea21c29df
Sebastian Cardozo <99739645+sebascardozo@users.noreply.github.com>
2022-05-17 14:29:43 -0300

    add buuton at card

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -8,11 +8,13 @@ function Item({product}) {
   return (
    <div className="card" >
     <Link to ={`imgine3d/detail/${product.id}`}>
-          <img className="card-image" src={product.image} alt="Imagen del producto"></img>  
-          <div>{product.name}</div>
+          <img className="card-image" src={product.image} alt="Imagen del producto"></img> 
+          <div className="container-btn">
+          <button className="btn">Más Información</button>  
+          </div>
     </Link>
-    
-          <div>{product.price}</div>
+          <div className="card-name">{product.name}</div>
+          <div className="card-price">{product.price}</div>
           <ItemCounter stock={product.stock} initial={product.initial} onAdd={(count)=> alert(`Has agregado ${count} producto/s a tu carrito`)}/>
     </div>
     
