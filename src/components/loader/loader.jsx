946390964f627cfea9219e6efe8011aeac834669
Sebastian Cardozo <99739645+sebascardozo@users.noreply.github.com>
2022-05-17 10:08:16 -0300

    style loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,10 +1,10 @@
 import { Waveform } from '@uiball/loaders'
-
+import '../styles/loader.css'
 
 
 function Loader() {
   return (
-    <div>
+    <div className='loader'>
         <Waveform 
            size={40}
            lineWeight={3.5}
