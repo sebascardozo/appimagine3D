2bc7d7d6974a38584f02129cfb3f7982cc19b08b
Sebastian Cardozo <99739645+sebascardozo@users.noreply.github.com>
2022-06-22 11:39:18 -0300

    add styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,7 +7,7 @@ import "../styles/Form.css"
 
 export const Form =()  => {
   const[dataForm, setDataForm] = useState({email: "", emailConfirm: "", name: "", lastName:"", phone: ""})
-  const {totalPrice, cartList, deleteCart} = useCartContext()
+  const { cartList, deleteCart} = useCartContext()
 
   async function generateOrder(e) {
       e.preventDefault()
@@ -30,11 +30,17 @@ export const Form =()  => {
 
           const db = getFirestore()
           const queryCollection = collection(db, "orders")
-      
+          const buy =order.products = cartList.map((product) => {
+            const name = product.name;
+            const price = product.price;
+            const qty = product.qty;
+            return { name, price, qty };
+          })
           addDoc(queryCollection, order)
           .then(resp =>
               swal({
                   title:`Gracias por tu compra, tu orden ha sido generada con el número ${resp.id}`,
+                  text: `Compraste : ${JSON.stringify(buy)}`,
                   icon: "success"}))
           .catch(err => console.log(err))
           .finally(() => deleteCart())
@@ -69,15 +75,17 @@ export const Form =()  => {
 
   return (
     <div>
-        <h2>Formulario</h2>
-        <div className="cartInfo">
+        <h2 className="title">Formulario</h2>
+        <div className="form">
             <input name='name' type="text" placeholder='Nombre' value={dataForm.name} onChange={handlerChange}></input>
             <input name='lastName' type="text" placeholder='Apellido' value={dataForm.lastName} onChange={handlerChange}></input>
             <input name='phone' type="text" placeholder='Teléfono' value={dataForm.phone} onChange={handlerChange}></input>
             <input name='email' type="email" placeholder='Email' value={dataForm.email} onChange={handlerChange}></input>
              <input name='emailConfirm' type="email" placeholder='Re-ingresa tu email' value={dataForm.emailConfirm} onChange={handlerChange}></input>
-        </div>
+       
         <button className="end-button" onClick={generateOrder}>Terminar compra</button>
+        </div>
     </div>
+
   )
 }
